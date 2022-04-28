3e4bd07533cac62e193578ff1c0c87815ed742a2
shadawo <ribe.thibaut@gmail.com>
2022-04-28 10:15:56 +0200

    add_aes_key_encryption

--- wasm_test/index.js
+++ wasm_test/index.js
@@ -1,14 +1,31 @@
-import init, { chiffre, crypt_aes_gcm_siv, decrypt_aes_gcm_siv } from "./pkg/chris_math.js";
+import init, { hash, crypt_aes_gcm_siv, decrypt_aes_gcm_siv, crypt_aes_key, decrypt_aes_key, generateRandomString, new_CryptedMessage } from "./pkg/chris_math.js";
 
-async function hash(mdp) {
+async function chiffre(mdp) {
     await init();
-    console.log(chiffre(mdp));
+    let clé = generateRandomString(32);
+    console.log("clé " + clé);
+    let messageDiv = document.getElementById("message");
+
+    messageDiv.addEventListener("change", async function (event) {
+        let message = messageDiv.value;
+        console.log(message);
+        let JSON = await send_crypted_message(message, mdp);
+        console.log(JSON);
+        extract_from_JSON(JSON, mdp);
+        //chiffre_dechiffre(message);
+    })
+
+    let cipherKey = crypt_aes_key(clé, mdp);
+    console.log("chiffre key aes: " + cipherKey);
+    console.log("dechiffre key aes " + decrypt_aes_key(cipherKey, mdp));
+
+    console.log("hash du mdp " + hash(mdp));
 }
 
 async function chiffre_dechiffre(messageAChiffrer) {
     await init()
-    let messageChiffre = await crypt_aes_gcm_siv(messageAChiffrer, "password");
-    console.log(messageChiffre);
+    let messageChiffre = await crypt_aes_gcm_siv(messageAChiffrer);
+    console.log(messageChiffre.key);
     let messageDechiffre = await decrypt_aes_gcm_siv(messageChiffre);
     console.log(messageDechiffre);
 }
@@ -19,14 +36,28 @@ let mdpDiv = document.getElementById("mdp");
 
 mdpDiv.addEventListener("change", function (event) {
     let mdp = mdpDiv.value;
-    console.log(mdp);
-    hash(mdp);
+    console.log("mdp :" + mdp);
+    chiffre(mdp);
+
 })
 
-let messageDiv = document.getElementById("message");
 
-messageDiv.addEventListener("change", function (event) {
-    let message = messageDiv.value;
-    console.log(message);
-    chiffre_dechiffre(message);
-})
+
+async function send_crypted_message(message, password) {
+    await init();
+    let cipher = crypt_aes_gcm_siv(message);
+    let key = cipher.get_key();
+    let crypted_key = crypt_aes_key(key, password);
+    let JSON = {
+        "messages": cipher.get_text(),
+        "key": crypted_key,
+        "nonce": cipher.get_nonce()
+    };
+    return JSON;
+};
+
+function extract_from_JSON(JSON, password) {
+    let cryptedMessage = new_CryptedMessage(JSON.messages, decrypt_aes_key(JSON.key, password), JSON.nonce);
+    let plainText = decrypt_aes_gcm_siv(cryptedMessage);
+    console.log("your messages : " + plainText);
+}
--- wasm_test/src/lib.rs
+++ wasm_test/src/lib.rs
@@ -1,14 +1,26 @@
+//use std::str::from_utf8;
+//use std::str::pattern::StrSearcher;
+
+//use aes::cipher::consts::{B1, B0};
+//use aes::cipher::typenum::{UInt, UTerm};
 use wasm_bindgen::prelude::*; 
-use hex_literal::hex;
+//use hex_literal::hex;
+//use aes::Aes256;
+/*use aes::cipher::{
+    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
+    generic_array::GenericArray,
+};*/
 use sha2::{Sha256, Digest};
 use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
 use aes_gcm_siv::aead::{Aead, NewAead};
+//use aes_gcm::{Aes256Gcm, Key, Nonce}; // Or `Aes128Gcm`
+//use aes_gcm::aead::{Aead, NewAead};
 use rand::{distributions::Alphanumeric, Rng};
 use pbkdf2::{
-    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    password_hash::{PasswordHasher},
     Pbkdf2
 };
-use rand_core::OsRng;
+//use rand_core::{OsRng, block};
 
 
 #[wasm_bindgen]
@@ -22,7 +34,7 @@ pub fn square(n1:i32)->i32{
 }
 
 #[wasm_bindgen]
-pub fn chiffre(mut password:String)->String{
+pub fn hash(mut password:String)->String{
       // create a Sha256 object
       const SALT:&str="a31158c6-9cb0-11ec-b909-0242ac120002";
 
@@ -42,14 +54,36 @@ pub fn chiffre(mut password:String)->String{
 pub struct CryptedMessage{
     cipherText: Vec<u8>,
     key: String,
-    nonce:Nonce, 
+    nonce:String, 
+}
+#[wasm_bindgen]
+pub fn new_CryptedMessage(cipherText: Vec<u8>,key: String,nonce:String)->CryptedMessage{
+    let crypted_message= CryptedMessage{
+        cipherText:cipherText,
+        key:key,
+        nonce:nonce
+    };
+    return crypted_message;
+}
+
+#[wasm_bindgen]
+impl CryptedMessage{
+    pub fn get_text(&self)->Vec<u8>{
+        self.cipherText.clone()
+    }
+    pub fn get_key(&self)->String{
+        self.key.clone()
+    }
+    pub fn get_nonce(&self)->String{
+        self.nonce.clone()
+    }
 }
 
 #[wasm_bindgen]
 pub fn pbkdf2_derivation(password : String) -> String{
     let password_to_hash = password.as_bytes();
-    let salt = SaltString::generate(&mut OsRng);
-    Pbkdf2.hash_password_simple(password_to_hash, salt.as_ref()).unwrap().to_string()
+    let salt = "hoBKFfPpeuP5OEO1UxaC42";
+    Pbkdf2.hash_password_simple(password_to_hash, salt).unwrap().to_string()
 }
 #[wasm_bindgen]
 pub fn truncate(s: &str, max_chars: usize) -> String {
@@ -69,21 +103,11 @@ pub fn generateRandomString(size:usize)->String{
 }
 
 #[wasm_bindgen]
-pub fn crypt_aes_gcm_siv(message:String,password:String)->CryptedMessage{
-    let secret_key=String::from(pbkdf2_derivation(password));
-    let  split=secret_key.split("$");
-    let vec: Vec<&str> = split.collect();
-    //println!("{}",secret_key);
-    let reduced_key=truncate(vec[4], 32);
-    //let key:&GenericArray<u8,32> = Key::from_slice(reduced_key.as_bytes());
+pub fn crypt_aes_gcm_siv(message:String)->CryptedMessage{
+   
 
     let rand_key=format!("{}",generateRandomString(32));
-    let aeskey = Key::from_slice(rand_key.as_bytes());
-    //let cle = format!("{:?}",key);
-    //println!("{}",cle);
-    
-    //let cipher = Aes256GcmSiv::new(key);
-    
+    let aeskey = Key::from_slice(rand_key.as_bytes());    
     let cipher = Aes256GcmSiv::new(aeskey);
 
     let rand_string=format!("{}",generateRandomString(12));
@@ -98,7 +122,7 @@ pub fn crypt_aes_gcm_siv(message:String,password:String)->CryptedMessage{
         cipherText: ciphertext,
         //key:reduced_key.to_string(),
         key:rand_key.to_string(),
-        nonce: *nonce,
+        nonce: rand_string,
     } ;
     
     return crypted_message;
@@ -107,7 +131,7 @@ pub fn crypt_aes_gcm_siv(message:String,password:String)->CryptedMessage{
 pub fn decrypt_aes_gcm_siv(crypted_message:CryptedMessage)->String{
     let key = Key::from_slice(crypted_message.key.as_bytes());
     let cipher = Aes256GcmSiv::new(key);
-    let nonce=crypted_message.nonce;
+    let nonce=Nonce::from_slice(crypted_message.nonce.as_bytes());
     let plaintext = cipher.decrypt(&nonce, crypted_message.cipherText.as_ref())
         .expect("decryption failure!");  // NOTE: handle this error to avoid panics!
 
@@ -115,3 +139,60 @@ pub fn decrypt_aes_gcm_siv(crypted_message:CryptedMessage)->String{
     println!("Clear text:{}",message);
     return message.to_string();
 }
+
+
+#[wasm_bindgen]
+pub fn crypt_aes_key(key:String,password : String)->Vec<u8>{
+    let secret_key=String::from(pbkdf2_derivation(password));
+    let  split=secret_key.split("$");
+    let vec: Vec<&str> = split.collect();
+    //println!("{}",secret_key);
+    let reduced_key=truncate(vec[4], 32);
+    let key_aes = Key::from_slice(reduced_key.as_bytes());
+    let cipher = Aes256GcmSiv::new(key_aes);
+
+  //  let mut block=GenericArray::from_slice(key.as_bytes()).clone();
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+   // let mut block2 = block.clone();
+
+    
+        let crypted_key = cipher.encrypt(nonce, key.as_ref())
+        .expect("encryption failure!");  // NOTE: handle this error to avoid panics!
+        // println!("encrypted text:{}",format!("{:?}",ciphertext));
+        return crypted_key;
+
+    
+    //let new_block=block;
+    
+}
+
+#[wasm_bindgen]
+pub fn decrypt_aes_key(crypted_key:Vec<u8>,password:String)->String{
+    let secret_key=String::from(pbkdf2_derivation(password));
+    let  split=secret_key.split("$");
+    let vec: Vec<&str> = split.collect();
+    //println!("{}",secret_key);
+    let reduced_key=truncate(vec[4], 32);
+    let key_aes = Key::from_slice(reduced_key.as_bytes());
+    let cipher = Aes256GcmSiv::new(key_aes);
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+    let key = cipher.decrypt(nonce, crypted_key.as_ref())
+    .expect("decription failure!");  // NOTE: handle this error to avoid panics!
+    return String::from_utf8_lossy(&key).to_string();
+
+}
+/*
+pub fn decrypt_aes_key(crypted_key:String,password : String)->String{
+    let secret_key=String::from(pbkdf2_derivation(password));
+    let  split=secret_key.split("$");
+    let vec: Vec<&str> = split.collect();
+    //println!("{}",secret_key);
+    let reduced_key=truncate(vec[4], 32);
+    let key_aes = GenericArray::from_slice(reduced_key.as_bytes());
+    let cipher = Aes256::new(&key_aes);
+    let mut block=GenericArray::from_slice(crypted_key.as_bytes());
+    cipher.decrypt_block(&mut block);
+    return String::from_utf8_lossy(block).to_string();
+
+ }
+*/
